4add162aa89419c98424175b1caf13597b0ad975
jreidgreer <jreid.greer@gmail.com>
2016-03-03 12:31:57 -0800

    Initalize init

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,39 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  var name = "node";
+  var counter = 0;
+ 
+
   list.addToTail = function(value) {
+    // Declare variable that initates Node
+    var node = this['node' + counter] = new Node(value);
+
+    //list.variable = variable;
+
+    //Do we have a list.head?
+    if (this.tail === null) {
+      //If not, list.head == list.variable
+      this.tail = node;
+
+
+    } else {
+      //Set list.variable.next = list.tail
+      
+    }
+
+
+
+    // Set list.tail to list.variable
+    counter++;
   };
 
   list.removeHead = function() {
+
   };
 
   list.contains = function(target) {
+
   };
 
   return list;
